06017652bf449308e881a99e7389e6da36dba03e
nkgrnkgr <beefupme@gmail.com>
2018-06-02 07:19:09 +0900

    add react-sass test

---
+++ test/react-sass/src/components/sub.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Sub extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>sub module</h3>
+      </div>
+    );
+  }
+}
+
+export default Sub;
